4710a8c2f31121004f8e2eed2330fd80b947238f
tianheg <yidajiabei@163.com>
2021-12-20 15:33:00 +0800

    modify: Mapbox API

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,6 +1,6 @@
 // const
 const MAPBOX_TOKEN =
-  'pk.eyJ1IjoieWlob25nMDYxOCIsImEiOiJja2J3M28xbG4wYzl0MzJxZm0ya2Fua2p2In0.PNKfkeQwYuyGOTT_x9BJ4Q';
+  'pk.eyJ1IjoiMTExMzkxMDIzOCIsImEiOiJja3hlZDFqazAzbjMxMm5vMXpjMmVsMTcxIn0.A5559SuTmBYEJSJhP_LYZw';
 const MUNICIPALITY_CITIES_ARR = [
   '北京市',
   '上海市',
